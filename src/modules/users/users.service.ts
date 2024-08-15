292c8f1583c748b3430bd37e8bcf9558e09155c8
VitaliiGBunakov <v.g.bunakov@gmail.com>
2024-08-15 20:45:05 +0300

    added utilities for hashing pass etc

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,24 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './entities/user.entity';
+import { PublicUserDto } from './dto/public-user.dto';
+import { UtilitiesService } from '../utilities/utilities.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User)
     private userModel: typeof User,
+    private utilities: UtilitiesService,
   ) {}
 
-  create(newUser: CreateUserDto): Promise<User> {
-    return this.userModel.create({
-      firstName: newUser.firstName,
-      lastName: newUser.lastName,
+  async create(newUser: CreateUserDto): Promise<PublicUserDto> {
+    const result = await this.userModel.create({
+      username: newUser.username,
+      password: await this.utilities.hashString(newUser.password),
+    });
+    const { password, ...createdUser } = result.toJSON();
+    return createdUser;
+  }
+
+  async findOne(id: number): Promise<PublicUserDto> {
+    return this.userModel.findOne({
+      where: { id },
+      attributes: ['id', 'username'],
     });
   }
 
-  findOne(id: number) {
+  async getFullUserByUsername(username: string): Promise<User> {
     return this.userModel.findOne({
-      where: { id },
+      where: {
+        username,
+      },
     });
   }
 }
